af5978b5a44af8c7aa7cc531e1085df6b88e2d12
prabhashrai02 <prabhashsahaj@gmail.com>
2022-01-03 23:19:44 +0530

    Integrated API and linked wirh search option

--- src/Homepage.js
+++ src/Homepage.js
@@ -4,8 +4,46 @@ import './Homepage.css';
 class Homepage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  
+            pictures: [],
+            textInput: "sen"
+        }
     }
+
+    componentDidMount(){
+        this.ReloadImage();
+    }
+
+    ReloadImage=()=>{
+        fetch('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e52a287d86469bf01ea901dfd92cf8a5&text=:'+this.state.textInput+'&media=photos&per_page=15&page=1&format=json&nojsoncallback=1')        
+        
+        .then(function(response){
+          return response.json();
+        })
+        .then(function(j){
+            let picArray = j.photos.photo.map((pic) => {
+                
+                var srcPath = 'https://farm'+pic.farm+'.staticflickr.com/'+pic.server+'/'+pic.id+'_'+pic.secret+'.jpg';
+                return(
+                <img src={srcPath}></img>
+                )
+            })
+            this.setState({pictures: picArray});
+        }.bind(this))
+    }
+
+    HandleChange=(e)=>{
+        this.setState({textInput: e.target.value});
+    }
+
+    Delay = (function(){
+        var timer=0;
+        return function(callback, ms){
+            clearTimeout(timer);
+            timer= setTimeout(callback, ms);
+        };
+    })();
+
     render() { 
         return (  
             <div>
@@ -16,10 +54,14 @@ class Homepage extends Component {
                         className='Navbar_search' 
                         type="text" 
                         placeholder='Search anything!!!'
+                        onChange={this.HandleChange}   
+                            onKeyUp={()=>this.Delay(function(){
+                                this.ReloadImage();
+                            }.bind(this), 0)}
                     />
                 </div>
                 <div className='Homepage_maincontent'>
-
+                    {this.state.pictures}
                 </div>
             </div>
         );
